4f805b35fdbc94458e0e51f8e7bb07cbdb4942a9
Jamshidbekh <jamshidmadaminjonov5@gmail.com>
2026-04-25 13:38:00 +0500

    masala va login

---
+++ script.js
@@ -0,0 +1,111 @@
+// let n= prompt("Son kiriting")
+// let m = prompt('Kattaroq son kiriting')
+// function gerv(a,b) {
+// 	for(let i=a;i<=b;i++){
+// 		if(i%3==0){
+// 		console.log(i);
+// 		}
+// 	}
+
+// }
+// gerv(n,m)
+
+// 2.Minutlarni butun son sifatida qabul qilib, uni sekundlarga
+// o’girib beradigan funksiya yasang.
+// let won = +prompt('Minut kiriting')
+// function toSekunds(min) {
+// 	return min * 60
+// }
+// console.log(toSekunds(won))
+// ==============================================================================================================
+
+// Funksiya butun son qabul qiladi. Funksiya ushbu butun
+// sondan keyingi sonni qaytarsin.
+
+// let int = 5
+// function nextInteger(int) {
+// 	return int + 1
+// }
+
+// console.log(nextInteger(int))
+// =========================================================================================================
+
+// Funskiya uchburchakning asosi va balandligini qabul qiladi.
+// Funksiya uchburchakning yuzini hisoblab qaytarsin. Uchburchak yuzini topish formulasi: S = (asos * balandlik) / 2
+
+// function uchburchakYuzi(asos, balandlik) {
+
+// 	return (asos*balandlik)/2
+
+// }
+// console.log(uchburchakYuzi(8,3));
+// // ==============================================================================================================
+
+// .Funksiya yoshni yil ko’rinishida qabul qiladi. ushbu funksiya
+// yoshni kunlarda hisoblab natijani qaytarsin. Bir yil 365 kun deb
+// oling.
+// function ageToDays(age) {
+
+// 	return age*365
+// }
+
+// console.log(ageToDays(65))
+// ======================================================================================================================
+
+// .Quyidagi namunalarga muvofiq ravishda funksiyani tana
+// qismini yozing.
+
+// function add(kub) {
+// 	return Math.pow(kub, 3)
+// }
+
+// console.log(add(3))
+// ====================================================================================================================
+
+// 8.Funksiya 2ta argument qabul qilsin. Ushbu funksiya birinchi
+// qiymatning ikkinchi qiymat bilan ko’paytmasini qaytarsin.
+
+// function power(x,y) {
+// return x*y
+// }
+
+
+// console.log(power(230, 10))
+// ==============================================================================================================
+// 9.Soat ko’rinishida qiymat kiritilsa, sekundlarda qaytaradigan
+// funksiya yasang.
+// function hourToSekunds(hour) {
+// return  hour*3600
+
+// }
+// console.log(hourToSekunds(2))
+// =====================================================================================================================
+
+// .Uchburchakning uchinchi tomonining eng uzun qiymatini
+// qaytaradigan funksiya yasang.
+// Formula: Eng uzun uchinchi tomon = (tomon1 + tomon2) - 1
+
+// function uchinchiTomon(tomon1, tomon2) {
+
+// return (tomon1 + tomon2) - 1
+
+// }
+// console.log(uchinchiTomon(8, 10))
+// ==========================================================================================================================================
+
+// Ikkita son argument sifatida kiritilsa, ushbu ikkala sonni birinchisini ikkinchisiga bo’lgandagi qoldiqni qaytarib beradigan
+// funksiya yasang.
+
+// function qoldiq(son1, son2) {
+// return son1%son2
+// }
+// console.log(qoldiq(10,3));
+// ==========================================================================================================================================
+// 12.Turtburchakning bo’yi va eni berilsa uning yuzini hisoblab
+// qaytaradigan funksiya yasang. Formula S = bo’yi * eni
+function turtburchakYuzi(boyi, eni) {
+
+
+}
+console.log(turtburchakYuzi(6, 7) 
+);
